ba500ebb2ed729382555c9b8fd68a3cd8f102688
rniochy <lniochy@gmail.com>
2022-10-06 21:10:38 +0100

    adding new features into js files

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const upArrow = document.getElementById("up-arrow");
+const right = document.getElementById("right-arrow");
+const leftArrow = document.getElementById("left-arrow");
+const downArrow = document.getElementById("down-arrow");
+
