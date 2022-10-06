4540475c111dc17b97b19e96d776275732d388df
rniochy <lniochy@gmail.com>
2022-10-06 21:12:29 +0100

    added features into js event

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 const upArrow = document.getElementById("up-arrow");
-const right = document.getElementById("right-arrow");
+const rightArrow = document.getElementById("right-arrow");
 const leftArrow = document.getElementById("left-arrow");
 const downArrow = document.getElementById("down-arrow");
 
+upArrow.addEventListener("click", ()=>{
+      alert();
+});
+upArrow.addEventListener("click", ()=>{
+      alert();
+});
+rightArrow.addEventListener("click", ()=>{
+      alert();
+});
+leftArrow.addEventListener("click", ()=>{
+      alert();
+});
+downArrow.addEventListener("click", ()=>{
+      alert();
+});
